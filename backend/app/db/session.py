ce738ac9d65cdf6bc459f618038dcad4543fe08f
Kansagara-Krish <kansagara.krish2006@gmail.com>
2026-04-08 11:26:45 +0530

    done

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,6 +1,9 @@
 import os
 from sqlmodel import SQLModel, create_engine, Session
+from sqlalchemy import text
 from dotenv import load_dotenv
+import psycopg2
+from urllib.parse import urlparse
 
 load_dotenv()
 
@@ -8,7 +11,28 @@ DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localho
 
 engine = create_engine(DATABASE_URL, echo=True)
 
+def create_database_if_not_exists():
+    url = urlparse(DATABASE_URL)
+    db_name = url.path.lstrip('/')
+    conn = psycopg2.connect(
+        host=url.hostname,
+        port=url.port or 5432,
+        user=url.username,
+        password=url.password,
+        database='postgres'
+    )
+    conn.autocommit = True
+    cur = conn.cursor()
+    try:
+        cur.execute(f"CREATE DATABASE {db_name}")
+    except psycopg2.errors.DuplicateDatabase:
+        pass
+    finally:
+        cur.close()
+        conn.close()
+
 def init_db():
+    create_database_if_not_exists()
     SQLModel.metadata.create_all(engine)
 
 def get_session():
